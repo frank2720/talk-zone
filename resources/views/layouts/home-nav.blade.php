4dc480e7b72cc4f4b55e47939c663f2d4a8eb9b7
frank2720 <otienof534@gmail.com>
2023-11-04 22:29:34 +0300

    landing page

--- resources/views/layouts/home-nav.blade.php
+++ resources/views/layouts/home-nav.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         {{ __('Home') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('talks.index')" :active="request()->routeIs('talks.index')">
+                        {{ __('Talks') }}
+                    </x-nav-link>
                 </div>
             </div>
 
