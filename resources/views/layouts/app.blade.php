9b65e049342ff554e1e55131613abf4671a6b6c2
frank2720 <otienof534@gmail.com>
2023-11-05 05:57:37 +0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!--favicon-->
+        <link rel="icon" type="image/x-icon" href="images/favicon.png">
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
